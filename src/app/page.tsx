174228c9610bf3d71a47f2ec81a6ff3bdc3779b8
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-10-06 11:28:54 -0700

    more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           height={38}
           priority
         />
-        
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -28,7 +27,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Publish Now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
